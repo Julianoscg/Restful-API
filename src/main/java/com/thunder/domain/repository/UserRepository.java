27c41f9f523f9456340bd1fafbb36e91a9bca719
Juliano Silva <julianosicg@gmail.com>
2024-07-25 15:30:03 -0300

    Implementação da camada de dominio

---
+++ src/main/java/com/thunder/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.thunder.domain.repository;
+
+import com.thunder.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
